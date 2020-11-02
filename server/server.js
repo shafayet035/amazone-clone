0fe940daca81d2fd9d93c20b94f55fb351dba08d
Shafayet Hossain <shafayet035@gmail.com>
2020-11-02 20:19:17 +0600

    amazone-clone

---
+++ server/server.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const cors = require("cors");
+const stripe = require("stripe")('sk_test_51HiitTBGurzF7kkBcI1JvTpG6Jr8vWGfRoMYCyMRNLvvJnfAQ8K3CDgnmduf3kG9eHKsLnAo7KhpuiyDhgPMYC9000lVr1aLxi')
+
+//API
+
+// app config
+const app = express()
+
+//Middlewares
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+//API routes
+app.get('/', (req, res) => {
+    res.send('Whats up?')
+})
+
+app.post('/payments/create', async (req, res) => {
+    const total = req.query.total;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd"
+    });
+
+    res.status(201).send({
+        clientSecret: paymentIntent.client_secret
+    })
+    console.log('Paymet Recieved total of', total)
+})
+
+const port = 5000
+
+//Litsen Commands
+app.listen(port, (req, res) => {
+    console.log(`App Running on Port${port}`)
+})
+
+// http://localhost:5001/e-clone-2c81e/us-central1/api
