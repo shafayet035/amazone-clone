004c2ac56838e17142e8c427f632b290121528f5
Shafayet Hossain <shafayet035@gmail.com>
2020-11-02 20:23:06 +0600

    make it better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header'
+import Home from './Components/Home/Home'
+import Checkout from './Components/Checkout/Checkout'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Auth from './Components/Auth/Auth';
+import { useEffect } from 'react';
+import { auth } from './firebase'
+import { useStateValue } from './Store/StateProvide';
+import Payment from './Components/Payment/Payment';
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import Orders from './Components/Orders/Orders';
+
+
+const promise = loadStripe     ("pk_test_51HiitTBGurzF7kkBFD48DrD68995qN7OnMv1vGVPmOjHo8CZuM025OoOoLcvMLpLHDGzwLdcrYvoUFyn4XDAXvid00vPOK2Xnk")
 
 function App() {
+  const [state, dispatch] = useStateValue()
+
+  useEffect(() => {
+    auth.onAuthStateChanged(authUser => {
+      console.log('Current user >', authUser)
+      if(authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }else {
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route path="/orders" exact>
+            <Orders />
+          </Route>
+          <Route path="/payment" exact>
+            <Elements stripe={promise}>
+             <Payment />  
+            </Elements>   
+          </Route>
+          <Route path="/authentication" exact component={Auth}/>
+          <Route path="/checkout" exact component={Checkout} />
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
